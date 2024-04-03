0db341521d4ae640ececaede2297e1a6ce7d638d
jfgrissom <jfgrissom@gmail.com>
2024-04-02 19:47:23 -0700

    add ic svg adds top menu mobile reactive layout

--- src/frontend/src/Layout/Footer/index.tsx
+++ src/frontend/src/Layout/Footer/index.tsx
@@ -1,10 +1,13 @@
+import iclogo from '../../assets/internet-computer-icp-logo.svg'
+
 export const Footer = () => {
   return (
-    <div className="bg-slate-800 col-span-12 h-[3.75rem] text-center p-2 text-sky-100">
+    <div className="bg-slate-800 col-span-12 min-h-[3.75rem] text-center p-4 text-sky-100">
       <p>
-        This project is proudly hosted on{' '}
+        This project is proudly hosted on the {' '}
         <a href="https://internetcomputer.org/" target="_blank" className="text-sky-400">
-          The Internet Computer
+          <img src={iclogo} alt="Internet Computer Logo" className="h-8 w-8 inline p-0 mb-1 align-middle" />{' '}
+          Internet Computer
         </a>{' '}
         made possible by the <a href="https://dfinity.org/" className="text-sky-400">
           DFINITY Foundation
