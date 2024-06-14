a3f5d8398c157c1e65e2b6b85f977d47b35b4a29
jfgrissom <jfgrissom@gmail.com>
2024-06-14 06:50:54 -0700

    add AssetPair provider

---
+++ src/frontend/src/Contexts/AssetPair/index.tsx
@@ -0,0 +1,38 @@
+import { useState, Dispatch, createContext, FC, PropsWithChildren } from "react"
+import { Icp, Usd, Asset } from "../../../fixtures/assets"
+
+interface AssetPair {
+  base: Asset
+  quote: Asset
+}
+
+const defaultAssetPair: AssetPair = {
+  base: Icp,
+  quote: Usd
+}
+
+interface AssetPairFocusContextType {
+  assetPair: AssetPair
+  setAssetPair: Dispatch<React.SetStateAction<AssetPair>> | null
+}
+
+const defaultContext: AssetPairFocusContextType = {
+  assetPair: defaultAssetPair,
+  setAssetPair: null
+}
+
+const PositionFocusContext = createContext<AssetPairFocusContextType>(defaultContext)
+
+// This component provides setters and getters for the AssetPair Context.
+export const AssetPairProvider: FC<PropsWithChildren> = (props) => {
+  const { children } = props
+
+  // Default value is our favorite asset pair.
+  const [assetPair, setAssetPair] = useState<AssetPair>(defaultAssetPair)
+
+  return (
+    <PositionFocusContext.Provider value={{ assetPair, setAssetPair }}>
+      {children}
+    </PositionFocusContext.Provider>
+  )
+}
