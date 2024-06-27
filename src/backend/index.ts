f338dd8ca754d808eb996c7afe1290e166d9847a
jfgrissom <jfgrissom@gmail.com>
2024-06-27 08:35:55 -0700

    add configuration for DNS on IC

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -13,6 +13,13 @@ app.use('/v1', router)
 // by the dfx.json configuration file.
 app.use(express.static('/dist'))
 
+// Delivers information required by IC to host our domain.
+app.use(
+  '/.well-known/ic-domains',
+  express.static('/dist/.well-known/ic-domains')
+)
+app.use('/.ic-assets.json', express.static('/dist/.ic-assets.json'))
+
 // This is the catch-all middleware that delivers the frontend.
 // The frontend is a single-page application (SPA) that presents errors to the user.
 app.use((_err: any, _req: Request, res: Response, _next: NextFunction) => {
