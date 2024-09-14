cc4829db08bc89f40664b8d3ce71239cb4ca77d1
jfgrissom <jfgrissom@gmail.com>
2024-09-14 08:10:24 -0700

    feat(providers): adds provider for backend integration using ic-react

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -1,7 +1,16 @@
-import { StrictMode } from 'react'
-import ReactDOM from 'react-dom/client'
 import './Styles/index.css'
+import { StrictMode } from 'react'
 import App from './App'
+import ReactDOM from 'react-dom/client'
+import {
+  ActorProvider,
+  AgentProvider
+} from '@ic-reactor/react'
+import {
+  idlFactory as backendIdlFactory,
+  canisterId as backendCanisterId
+} from '../../declarations/backend'
+import { ErrorPage } from './Components/Error'
 
 // This is the root element that the React app will be mounted to
 const root = document.getElementById('root')
@@ -9,6 +18,17 @@ const root = document.getElementById('root')
 // Render the React app
 ReactDOM.createRoot(root!).render(
   <StrictMode>
-    <App />
-  </StrictMode >,
+    <AgentProvider withLocalEnv port={8000}>
+      {/* <AgentProvider> // Production */}
+      <ActorProvider idlFactory={backendIdlFactory}
+        canisterId={backendCanisterId}
+        errorComponent={() => (
+          <ErrorPage
+            errorMessage=""
+          />
+        )}>
+        <App />
+      </ActorProvider>
+    </AgentProvider>
+  </StrictMode >
 )
