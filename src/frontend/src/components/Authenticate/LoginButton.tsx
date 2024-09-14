3aac3dd55456ec3687fa82704e6f5bb3bab1dca3
jfgrissom <jfgrissom@gmail.com>
2024-09-14 16:22:34 -0700

    feat(auth): adds options for logout or profile to auth button

--- src/frontend/src/components/Authenticate/LoginButton.tsx
+++ src/frontend/src/components/Authenticate/LoginButton.tsx
@@ -1,6 +1,14 @@
 import { FC } from 'react'
 import iclogo from '../../Assets/internet-computer-icp-logo.svg'
 import { useAuth } from "@ic-reactor/react"
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
+
 
 interface LoginButtonProps {
   dark?: boolean
@@ -33,13 +41,30 @@ export const LoginButton: FC<LoginButtonProps> = (props) => {
     )
   }
 
-  return <a
-    href="/"
-    className={`-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7
-                 text-sky-100 hover:bg-slate-700 ${dark ? 'bg-slate-800 hover:bg-slate-900' : null}`}
-    onClick={() => { logout() }}
-  >
-    <img src={iclogo} className="h-8 w-8 inline p-0 mb-1 mr-2 align-middle" />
-    Logout ({identity?.getPrincipal().toString().slice(0, 6)}...{identity?.getPrincipal().toString().slice(-4)})
-  </a>
+  return (
+    <Select>
+      <SelectTrigger className="w-[180px] bg-black text-white">
+        <img src={iclogo} className="h-8 w-8 inline p-0 mb-1 mr-2 align-middle" />
+        {identity?.getPrincipal().toString().slice(0, 6)}...{identity?.getPrincipal().toString().slice(-4)}
+        {/* <SelectValue>Testing</SelectValue> */}
+      </SelectTrigger>
+      <SelectContent className="bg-black text-white">
+        <SelectItem value="light">Profile</SelectItem>
+        <SelectItem value="system">Logout</SelectItem>
+      </SelectContent>
+    </Select>
+
+  )
+
+
+
+  // <a
+  //   href="/"
+  //   className={`-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7
+  //                text-sky-100 hover:bg-slate-700 ${dark ? 'bg-slate-800 hover:bg-slate-900' : null}`}
+  //   onClick={() => { logout() }}
+  // >
+  //   <img src={iclogo} className="h-8 w-8 inline p-0 mb-1 mr-2 align-middle" />
+  //   Logout ({identity?.getPrincipal().toString().slice(0, 6)}...{identity?.getPrincipal().toString().slice(-4)})
+  // </a>
 }
